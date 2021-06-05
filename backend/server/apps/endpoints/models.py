8bd3fd06a6bad5b2ac1f3df3a53fd172a84e880f
IndraP24 <indrashis985@gmail.com>
2021-06-05 20:21:46 +0530

    Create MLAlgo status model

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -36,3 +36,25 @@ class MLAlgorithm(models.Model):
     owner = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+
+
+    class MLAlgorithmStatus(models.Model):
+        '''
+        The MLAlgorithmStatus represent status of the MLAlgorithm which can change during the time.
+
+        Attributes:
+            status: The status of algorithm in the endpoint. Can be: testing, staging, production, ab_testing.
+            active: The boolean flag which point to currently active status.
+            created_by: The name of creator.
+            created_at: The date of status creation.
+            parent_mlalgorithm: The reference to corresponding MLAlgorithm.
+
+        '''
+        status = models.CharField(max_length=128)
+        active = models.BooleanField()
+        created_by = models.CharField(max_length=128)
+        created_at = models.DateTimeField(auto_now_add=True, blank=True)
+        parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name = "status")
+    
+
+    
