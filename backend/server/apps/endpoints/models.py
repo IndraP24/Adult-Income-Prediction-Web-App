a4143981d05c2cc06d996f06457e745b0e91cfe3
IndraP24 <indrashis985@gmail.com>
2021-06-05 20:19:33 +0530

    Create MLAlgorithm model

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -14,3 +14,25 @@ class Endpoint(models.Model):
     name = models.CharField(max_length=128)
     owner = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class MLAlgorithm(models.Model):
+    '''
+    The MLAlgorithm represent the ML algorithm object.
+
+    Attributes:
+        name: The name of the algorithm.
+        description: The short description of how the algorithm works.
+        code: The code of the algorithm.
+        version: The version of the algorithm similar to software versioning.
+        owner: The name of the owner.
+        created_at: The date when MLAlgorithm was added.
+        parent_endpoint: The reference to the Endpoint.
+    '''
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
