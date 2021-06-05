ae6962f45215b3bd8e8840bb3ad65d41a5518862
IndraP24 <indrashis985@gmail.com>
2021-06-05 20:17:38 +0530

    Create Endpoint

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Endpoint(models.Model):
+    '''
+    The Endpoint object represents ML API endpoint
+
+    Attributes:
+        name: The name of the endpoint, it will be used in API url,
+        owner: The string with ownner name,
+        created_at: The date when endpoint was created.
+    '''
+    name = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
